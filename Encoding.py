5ad25c7bad08f245150cb57062fe725b0402d917
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2021-03-03 13:40:27 -0500

    The main files are here!

---
+++ Encoding.py
@@ -0,0 +1,142 @@
+import string
+from base64 import b64encode as b64e, b64decode as b64d
+from collections import deque
+from random import randint as r, randrange as rr
+from tkinter import *
+from tkinter import messagebox
+from encodelib import *
+
+
+def encode_and_show():
+    outbox.config(state="normal")
+    outbox1.config(state="normal")
+    outbox2.config(state="normal")
+    normal_key.config(state="normal")
+    complex_key.config(state="normal")
+    in_text = input_box.get(1.0, "end-1c")
+    encoded_simple = begin(in_text, "e")
+    encoded_complex = begin(in_text, "ec")
+    outbox.delete(1.0, END)
+    outbox.insert(1.0, encoded_simple[0])
+    normal_key.delete(1.0, "end-1c")
+    normal_key.insert(1.0, encoded_simple[1])
+    outbox1.delete(1.0, END)
+    outbox1.insert(1.0, encoded_complex[0])
+    complex_key.delete(1.0, "end-1c")
+    complex_key.insert(1.0, str(encoded_complex[1]).strip("[]").replace(", ", ",") + "\n" + str(encoded_complex[2]) +
+                       "\n" + str(encoded_complex[3]))
+    if in_text.__len__() > 25:
+        complex_key.config(width=50)
+    else:
+        complex_key.config(width=in_text.__len__() * 2 - 1)
+    outbox2.delete(1.0, END)
+    outbox2.insert(1.0, b64e(in_text.encode()).decode())
+    outbox.config(state="disabled")
+    outbox1.config(state="disabled")
+    outbox2.config(state="disabled")
+    normal_key.config(state="disabled")
+    complex_key.config(state="disabled")
+
+
+def decode_and_show():
+    decode_outbox.config(state="normal")
+    decode_outbox.delete(1.0, "end-1c")
+    extra_info_s = decode_extra_info.get(1.0, "end-1c")
+    if extra_info_s.strip() == "" and current_decode_choice.get() != "Base64":
+        messagebox.showerror(title="Decoding error", message="Wrong decoding type! Did you mean Base64?")
+        current_decode_choice.set("Base64")
+        return
+    elif extra_info_s.split("\n").__len__() == 3 and current_decode_choice.get() != "Complex":
+        messagebox.showerror(title="Decoding error", message="Wrong decoding type! Did you mean Complex?")
+        current_decode_choice.set("Complex")
+        return
+    elif extra_info_s.split("\n").__len__() == 1 and current_decode_choice.get() != "Normal" and extra_info_s.strip() \
+            != "":
+        messagebox.showerror(title="Decoding error", message="Wrong decoding type! Did you mean Normal?")
+        current_decode_choice.set("Normal")
+        return
+    if current_decode_choice.get() == "Normal":
+        key = int(extra_info_s)
+        decode_outbox.insert(1.0, _decode(input_d_box.get(1.0, "end-1c"), key))
+    elif current_decode_choice.get() == "Complex":
+        scrambled_nums_list = [int(i) for i in extra_info_s.split("\n")[0].split(",")]
+        scramble_key = int(extra_info_s.split("\n")[1])
+        key = int(extra_info_s.split("\n")[2])
+        decode_outbox.insert(1.0, _decode2(input_d_box.get(1.0, "end-1c"), key, scrambled_nums_list, scramble_key))
+    elif current_decode_choice.get() == "Base64":
+        decode_outbox.insert(1.0, b64d(input_d_box.get(1.0, "end-1c").encode()).decode())
+    decode_outbox.config(state="disabled")
+
+
+def switch_to_decode():
+    for item_ in encoding_items:
+        item_.grid_remove()
+    for item_ in decoding_items:
+        item_.grid()
+
+
+def switch_to_encode():
+    for item_ in decoding_items:
+        item_.grid_remove()
+    for item_ in encoding_items:
+        item_.grid()
+
+
+numbers_to_special_characters_key = [")", "!", "@", "#", "$", "%", "^", "&", "*", "("]
+if __name__ == '__main__':
+    app = Tk()
+    app.title("Encode/Decode")
+    app.minsize(1050, 225)
+    encodings = ["Normal", "Complex", "Base64"]
+    encode_button = Button(master=app, text="Press to encode", command=encode_and_show)
+    encode_button.grid(row=2, column=1, ipadx=50)
+    decode_button = Button(master=app, text="Press to decode", command=decode_and_show)
+    decode_button.grid(row=1, column=1, ipadx=50, pady=15)
+    input_box = Text(master=app, height=3, width=50)
+    input_box.grid(row=3, column=1)
+    normal_label = Label(master=app, text="Normal:  ")
+    normal_label.grid(row=2, column=2)
+    complex_label = Label(master=app, text="Complex:  ")
+    complex_label.grid(row=3, column=2)
+    base64_label = Label(master=app, text="Base64:  ")
+    base64_label.grid(row=4, column=2)
+    outbox = Text(master=app, height=3, width=50, borderwidth=0, state="disabled")
+    outbox.grid(row=2, column=3)
+    outbox1 = Text(master=app, height=3, width=50, borderwidth=0, state="disabled")
+    outbox1.grid(row=3, column=3)
+    outbox2 = Text(master=app, height=3, width=50, borderwidth=0, state="disabled")
+    outbox2.grid(row=4, column=3)
+    complex_encoding_info_label = Label(master=app, text="Info for complex encoding: ")
+    complex_encoding_info_label.grid(row=3, column=4)
+    normal_encoding_info_label = Label(master=app, text="Info for normal encoding: ")
+    normal_encoding_info_label.grid(row=2, column=4)
+    normal_key = Text(master=app, height=1, width=2, borderwidth=0, state="disabled")
+    normal_key.grid(row=2, column=5)
+    complex_key = Text(master=app, height=3, width=1, borderwidth=0, state="disabled")
+    complex_key.grid(row=3, column=5)
+    current_decode_choice = StringVar(app)
+    current_decode_choice.set(encodings[0])
+    encoding_option = OptionMenu(app, current_decode_choice, *encodings)
+    encoding_option.grid(row=3, column=1)
+    input_d_box = Text(master=app, height=3, width=50)
+    input_d_box.grid(row=2, column=1)
+    decode_outbox = Text(master=app, height=3, width=50, borderwidth=0, state="disabled")
+    decode_outbox.grid(row=2, column=3)
+    output_label = Label(master=app, text="Output: ")
+    output_label.grid(row=2, column=2)
+    decode_extra_info = Text(master=app, height=3, width=50)
+    decode_extra_info.grid(row=5, column=1)
+    decode_extra_info_label = Label(master=app, text="Extra info for the decoding: ")
+    decode_extra_info_label.grid(row=4, column=1)
+    switch_to_decode_button = Button(master=app, text="Switch to decoding", command=switch_to_decode)
+    switch_to_decode_button.grid(row=5, column=4, pady=15)
+    switch_to_encode_button = Button(master=app, text="Switch to encoding", command=switch_to_encode)
+    switch_to_encode_button.grid(row=5, column=4, pady=15)
+    encoding_items = [encode_button, input_box, normal_label, complex_label, base64_label, outbox, outbox1, outbox2,
+                      complex_encoding_info_label, complex_key, normal_encoding_info_label, normal_key,
+                      switch_to_decode_button]
+    decoding_items = [decode_button, input_d_box, encoding_option, decode_outbox, output_label, decode_extra_info,
+                      decode_extra_info_label, switch_to_encode_button]
+    for item in decoding_items:
+        item.grid_remove()
+    app.mainloop()
---
+++ encodelib.py
@@ -0,0 +1,159 @@
+import string
+from base64 import b64encode as b64e, b64decode as b64d
+from collections import deque
+from random import randint as r, randrange as rr
+from typing import Union
+
+
+def _base64encode(s):
+    return b64e(s.encode()).decode()
+
+
+def _base64decode(s):
+    return b64d(s.encode()).decode()
+
+
+def _encode(s, max_char_len: int, encode_special_characters=True):
+    encoded_return = ""
+    for character in s:
+        if encode_special_characters:
+            encoded_return += _numbers_to_special_characters(str(ord(character)).zfill(max_char_len))
+        else:
+            if character in string.ascii_letters + string.digits:
+                encoded_return += _numbers_to_special_characters(str(ord(character)).zfill(max_char_len))
+            else:
+                encoded_return += character
+    return encoded_return
+
+
+def _decode(s, max_char_len_: int):
+    decoded_return = ""
+    every_three = _split_into_list_of_len(s, max_char_len_)
+    for characters in every_three:
+        decoded_return += chr(_special_characters_to_numbers(characters))
+    return decoded_return
+
+
+def _encode2(s: str, max_char_len_: int):
+    numbers_list = [ord(n) for n in s]
+    rand_numbers_list = [r(1, 9) for _ in range(s.__len__())]
+    shift_key = rr(0, 2 ** (s.__len__() // 2))
+    shift_key_iter = bin(shift_key).lstrip("-0b").zfill(numbers_list.__len__() // 2)
+    multiplied_numbers_list = []
+    numbers_list_shuffled = _shuffle(shift_key_iter, rand_numbers_list)
+    return_s = ""
+    for n in range(len(numbers_list)):
+        multiplied_numbers_list.append(str(numbers_list[n] * rand_numbers_list[n]).zfill(max_char_len_ + 1))
+    for n in multiplied_numbers_list:
+        return_s += _numbers_to_special_characters(n)
+    return return_s, numbers_list_shuffled, shift_key, max_char_len_ + 1
+
+
+def _decode2(s: str, max_char_len_: int, shuffled_list: list, shuffle_key_: int):
+    return_string = ""
+    special_char_list = [_special_characters_to_numbers(c) for c in _split_into_list_of_len(s, max_char_len_)]
+    un_shuffled_list = _shuffle(bin(shuffle_key_).lstrip("-0b").zfill(shuffled_list.__len__() // 2), shuffled_list)
+    number_list = [special_char_list[n] // un_shuffled_list[n] for n in range(special_char_list.__len__())]
+    for char in number_list:
+        return_string += chr(char)
+    return return_string
+
+
+def _numbers_to_special_characters(number):
+    number_string = str(number)
+    result = ""
+    for character in number_string:
+        result += numbers_to_special_characters_key[int(character)]
+    return result
+
+
+def _special_characters_to_numbers(characters: str):
+    result = ""
+    for character in characters:
+        result += str(numbers_to_special_characters_key.index(character))
+    return int(result)
+
+
+def _test_input_yes_no(prompt: str, tries: int = 5, fail_message: str = "{} are not options, please try again! (Only "
+                                                                        "yes, y, no, and n are acceptable") -> bool:
+    if tries <= 0:
+        raise ValueError("Can't try 0 or less times!")
+    while True:
+        all_tries = []
+        for this_try in range(tries):
+            last_answer = input(prompt).lower()
+            if last_answer in ["y", "ye", "yes"]:
+                return True
+            elif last_answer in ["n", "no"]:
+                return False
+            all_tries.append(this_try)
+        print(fail_message.format(all_tries))
+
+
+def begin(s: str, types: str, encode_special_characters: bool = True) -> Union[tuple[str, list, int, int], str]:
+    encodings = _get_reps(types, ["e", "d", "c", "b"], ["encoding", "decoding", "complex", "base64"])
+    if encodings[0] == "" and encodings[1] == "" and encodings[2] != "":
+        raise ValueError("You need to specify one of either decode or encode with complex")
+    elif encodings[0] == "" and encodings[1] == "" and encodings[3] != "":
+        raise ValueError("You need to specify one of either decode or encode with base64")
+    elif encodings[0] != "" and encodings[1] != "":
+        raise ValueError("Cannot encode and decode at the same time")
+    elif encodings[3] != "" and encodings[2] != "":
+        raise ValueError("Can't be complex and base64 at the same time!")
+    elif encodings[0] != "" and encodings[2] != "":
+        return _encode2(s, _get_max_char_len(s))
+    elif encodings[0] != "" and encodings[2] == "" and encodings[3] != "":
+        return _encode(s, _get_max_char_len(s), encode_special_characters)
+    elif encodings[0] != "" and encodings[3] != "":
+        return _base64encode(s)
+    elif encodings[1] != "" and encodings[3] != "":
+        return _base64decode(s)
+
+
+def _split_into_list_of_len(_input: str, length: int):
+    result = []
+    __input = deque(_input)
+    for character in range(len(__input) // length):
+        this_every_three_append = ""
+        for _ in range(length):
+            this_every_three_append += __input.popleft()
+        result.append(this_every_three_append)
+    return result
+
+
+def _get_max_char_len(s: str) -> int:
+    return_int = 0
+    for char in s:
+        if str(ord(char)).__len__() > return_int:
+            return_int = str(ord(char)).__len__()
+    return return_int
+
+
+def _shuffle(shift_key_iter, numbers_list):
+    numbers_list_shuffled = []
+    for n in range(len(shift_key_iter)):
+        if shift_key_iter[n] == "1":
+            numbers_list_shuffled.append(numbers_list[n * 2 + 1])
+            numbers_list_shuffled.append(numbers_list[n * 2])
+        else:
+            numbers_list_shuffled.append(numbers_list[n * 2])
+            numbers_list_shuffled.append(numbers_list[n * 2 + 1])
+    if numbers_list.__len__() % 2 == 1:
+        numbers_list_shuffled.append(numbers_list[-1])
+    return numbers_list_shuffled
+
+
+def _get_reps(s: str, reps: list[str], reps_keys: list[str]) -> list[str]:
+    return_list = ["" for _ in range(len(reps))]
+    if len(reps) != len(reps_keys):
+        raise ValueError(f"reps needs to be the same length as reps_keys")
+    for char in s:
+        if char not in reps:
+            raise ValueError(f"{char}: invalid option")
+    for i in range(len(reps)):
+        if reps[i] in s:
+            return_list[i] = reps_keys[i]
+    return return_list
+
+
+numbers_to_special_characters_key = [")", "!", "@", "#", "$", "%", "^", "&", "*", "("]
